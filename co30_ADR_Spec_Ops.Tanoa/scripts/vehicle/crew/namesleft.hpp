0c2fb852cbe19ae77f00aba9dbdcf2eda221df68
Toxa Bes <toxabes@gmail.com>
2017-05-19 20:49:32 +0300

    names left position #274

--- co30_ADR_Spec_Ops.Tanoa/scripts/vehicle/crew/namesleft.hpp
+++ co30_ADR_Spec_Ops.Tanoa/scripts/vehicle/crew/namesleft.hpp
@@ -17,7 +17,7 @@ class HudNames {
 			type = CT_STRUCTURED_TEXT;
 			size = 0.040;
 			x = (SafeZoneX + 0.015);
-			y = (SafeZoneY + 0.60);
+			y = (SafeZoneY + 0.55);
 			w = 0.4; h = 0.65;
 			colorText[] = {1,1,1,1};
 			lineSpacing = 3;
